ab3da1a1b4004f34474cc7eefe1f025e2af1e22e
Jose Lavado <jose.lavado2016@gmail.com>
2024-10-25 10:04:18 -0400

    added role

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
-Hello
+#Provider Block
+provider "aws" {
+    profile = "default"
+    region = "ca-central-1"
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = "SSMRoleForEC2"
+}
+
+#Resources Block
+resource "aws_instance" "TF_app_server" {
+ami = "ami-074c42dd5ddb28c14"
+instance_type = "t2.micro"
+iam_instance_profile = aws_iam_instance_profile.test_profile.name
+tags ={
+    Name = "TF_EC2_app_server"
+    Owner ="Jose Lavado"
+}
+
+}
