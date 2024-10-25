4d56ded84f0a694a6210fca1f65dd71de7389dac
Jose Lavado <jose.lavado2016@gmail.com>
2024-10-25 10:23:04 -0400

    test1

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "aws_instance" "TF_app_server" {
 ami = "ami-074c42dd5ddb28c14"
 instance_type = "t2.micro"
 iam_instance_profile = aws_iam_instance_profile.test_profile.name
+
 tags ={
     Name = "TF_EC2_app_server"
     Owner ="Jose Lavado"
